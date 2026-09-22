b5fdb1fe5c1d1f0c341b2c9121104ca7851951e3
Noah Bumgardner <noah@playgamecafe.com>
2026-09-22 17:10:49 -0500

    Render Board of 14 Wide by 5 High
    Use Style from boardgame.io Tutorial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 import { Client } from "boardgame.io/client";
-import { VolleyballReturns } from "./game";
+import { board_height, board_width, VolleyballReturns } from "./game";
 
 class VolleyballReturnsClient {
-  constructor() {
+  constructor(rootElement) {
     this.client = Client({ game: VolleyballReturns });
     this.client.start();
+
+    // Build client side view.
+    this.rootElement = rootElement;
+    this.createBoard();
+  }
+
+  createBoard() {
+    // Create cells in rows for the Volleyball Returns board.
+    const rows = [];
+    for (let i = 0; i < board_height; i++) {
+      const cells = [];
+      for (let j = 0; j < board_width; j++) {
+        const id = 3 * i + j;
+        cells.push(`<td class="cell" data-id="${id}"></td>`);
+      }
+      rows.push(`<tr>${cells.join("")}</tr>`);
+    }
+
+    // Add the HTML to our app <div>.
+    // We’ll use the empty <p> to display the game winner later.
+    this.rootElement.innerHTML = `
+      <table>${rows.join("")}</table>
+      <p class="winner"></p>
+    `;
   }
 }
 
-const app = new VolleyballReturnsClient();
+const appElement = document.getElementById('app');
+new VolleyballReturnsClient(appElement);
--- src/game.js
+++ src/game.js
@@ -3,13 +3,13 @@ import { INVALID_MOVE } from 'boardgame.io/core';
 const board_height_in_bounds = 3;
 const board_height_out_bounds_half = 1;
 const board_height_out_bounds = board_height_out_bounds_half * 2;
-const board_height = board_height_in_bounds + board_height_out_bounds;
+export const board_height = board_height_in_bounds + board_height_out_bounds;
 
 const board_width_in_bounds_half = 6;
 const board_width_out_bounds_half = 1;
 const board_width_half =
   board_width_in_bounds_half + board_width_out_bounds_half;
-const board_width = board_width_half * 2;
+export const board_width = board_width_half * 2;
 
 const board_area = board_width * board_height;
 
